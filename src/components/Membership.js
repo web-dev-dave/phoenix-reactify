d886cc1aa24b62dd18b9759052243e6480d570fe
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-28 15:13:45 +1300

    start memebership page

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Membership() {
   return (
-    <>
-      <h1>Membership Page</h1>
-    </>
+    <div className='membership'>
+      <h1>Pricing</h1>
+      <p>
+        Our mission is to train individuals in constantly varied, functional
+        movements, at a high intensity in order for them to become their best
+        both physically and mentally. Preparing them for both the unknown and
+        the unknowable.
+      </p>
+    </div>
   );
 }
 
