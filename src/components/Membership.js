062f2856e63dbd10aa619984b49df2a93d3edcda
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-03 11:16:36 +1300

    start membershp page fresh

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,39 +1,57 @@
 import React from 'react';
-import barbell from '../assets/img/barbell.jpg';
 
 const Membership = () => {
   return (
-    <>
-      <section className='testamonials'>
-        <p className='text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-          excepturi! Exercitationem error inventore nam labore temporibus,
-          corrupti incidunt officia odio?
+    <div className='container'>
+      <div className='box'>
+        <h3>Box One</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
         </p>
-      </section>
-      <article className='membership'>
-        <img src={barbell} alt='lifting barbell' />
-        <p className='text'>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed amet
-          delectus necessitatibus architecto expedita voluptas officia sit, eos
-          voluptate ratione placeat? Veniam atque optio neque. Distinctio
-          suscipit quod adipisci? Aut? Aut fugiat velit nulla quibusdam soluta
-          quisquam aspernatur perferendis dicta architecto aperiam a cum iste
-          ducimus voluptate voluptatem, est beatae sapiente quo quod nisi? Eaque
-          ullam eum quidem sed ipsa. Laudantium asperiores doloremque eaque
-          provident architecto autem, hic explicabo sunt porro id minima
-          aliquid, et rerum repellat nostrum deleniti numquam? Eos reiciendis
-          totam fugiat temporibus. Eos cumque eaque quibusdam illo?
-          Reprehenderit amet, laboriosam harum doloribus nostrum ex sapiente
-          voluptates. Minima at ipsa nemo quis. Officiis error ratione adipisci
-          repellendus dignissimos totam, excepturi sint ex explicabo dicta est,
-          sunt cum numquam? Porro animi vel, adipisci necessitatibus distinctio
-          laborum quia. Consequuntur suscipit facilis, debitis vel esse ipsum.
-          Quas nam quaerat sint temporibus exercitationem ipsa, suscipit placeat
-          vero fugit? Ea at soluta sequi?
+      </div>
+      <div className='box'>
+        <h3>Box Two</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
         </p>
-      </article>
-    </>
+      </div>
+      <div className='box'>
+        <h3>Box Three</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
+        </p>
+      </div>
+      <div className='box'>
+        <h3>Box Four</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
+        </p>
+      </div>
+      <div className='box'>
+        <h3>Box Five</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
+        </p>
+      </div>
+      <div className='box'>
+        <h3>Box Six</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quisquam molestiae rem fugit! Libero delectus earum inventore tempore
+          debitis ipsa?
+        </p>
+      </div>
+    </div>
   );
 };
 
